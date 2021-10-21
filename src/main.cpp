ed82d27bc6bbc434c0369b8e083beb5df7bc3070
RedSnoww12 <amarasacha2@gmail.com>
2021-10-21 12:18:29 +0200

    Rework du snake

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,98 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cstdlib>
 
-#define WIDTH 800
-#define HEIGHT 800
-#define VITESSE 5
+using namespace std;
+using namespace sf;
 
-int main()
-{
-  sf::RenderWindow window(sf::VideoMode(WIDTH,HEIGHT), "Snake !");
-  window.setFramerateLimit(60);
-  
-  sf::RectangleShape rectangle(sf::Vector2f(10, 10));
-  rectangle.setFillColor(sf::Color::Blue);
-  sf::CircleShape shape(5.f);
-  shape.setFillColor(sf::Color::Red);
+int size = 30;
+int width = size*size, height = size*size;
+int num = 3;
+int dir = 1;
 
-  float x = 20, y = 20;
-  rectangle.setPosition(x,y);
-  shape.setPosition(400.f,400.f);
-  
-  window.draw(shape);
-  window.display();
-  
-  while (window.isOpen())
-    {
-      sf::Event event;
-      while (window.pollEvent(event)) {
-	if (event.type == sf::Event::Closed)
-	  window.close();
-      }
+struct snake{
 
-      /*sf::SoundBuffer buffer;
-      if (!buffer.loadFromFile("sound.wav"))
-        return -1;*/
+  int x,y;
+	   
+}s[100];
+
+struct fruit{
+  int x,y;
+}f;
+
+void Action(){
+
+  for(int i = num; i>0 ; i--){
+
+    s[i].x = s[i-1].x;
+    s[i].y = s[i-1].y;
       
-      window.draw(rectangle);
-      window.display();
+  }
 
-      if (x <= 790) {
-	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-	  while ( x <= 790) {
-	    rectangle.setPosition(x,y);
-	    window.clear();
-	    window.draw(rectangle);
-	    window.draw(shape);
-	    window.display();
-	    x = x + VITESSE;
-	    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))) {
-	      break;
-	    }
-	  }
-	}
-      }
+  if (dir == 0) s[0].y -= 1;
+  if (dir == 1) s[0].x += 1;
+  if (dir == 2) s[0].y += 1;
+  if (dir == 3) s[0].x -= 1;
 
-      if (y <= 790) {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-	  while ( y <= 790) {
-	    rectangle.setPosition(x,y);
-	    window.clear();
-	    window.draw(rectangle);
-	    window.draw(shape);
-	    window.display();
-	    y = y + VITESSE;
-	    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))) {
-	      break;
-	    }
-	  }
-	}
-      }
-
-      if (x >= 0) {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-	  while (x >= 0) {
-	    rectangle.setPosition(x,y);
-	    window.clear();
-	    window.draw(rectangle);
-	    window.draw(shape);
-	    window.display();
-	    x = x - VITESSE;
-	    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))) {
-	      break;
-	  }
-	  }
-	}
-      }
-
-      if (y >= 0) {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-	  while (y >= 0) {
-	    rectangle.setPosition(x,y);
-	    window.clear();
-	    window.draw(rectangle);
-	    window.draw(shape);
-	    window.display();
-	    y = y - VITESSE;
-	    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))) {
-	      break;
-	    }
-	  }
-	}
-      }
+  if (s[0].x == f.x && s[0].y == f.y)
+    {
+      num++;
+      f.x = rand()*size;
+      f.y = rand()*size;
     }
+}
 
+
+int main () {
+
+  RenderWindow app(VideoMode(width,height),"Snake", Style::Titlebar);
+  app.setFramerateLimit(60);
+
+  RectangleShape snake(Vector2f(size,size));
+  snake.setFillColor(Color::Blue);
+
+  RectangleShape fruit(Vector2f(size,size));
+  fruit.setFillColor(Color::Red);
+
+  
+  Clock timer;
+
+  f.x = rand()*size;
+  f.y = rand()*size;
+  
+  while(app.isOpen()) {
+    Event event;
+    while(app.pollEvent(event)){
+      switch(event.type)
+      case Event::KeyReleased:
+	if(event.key.code == Keyboard::Escape)
+	  app.close();
+      break;
+    }
+  }
+
+  if (timer.getElapsedTime().asMilliseconds()>200)
+    {
+      Action();
+      timer.restart();
+    }
+  
+  if(Keyboard::isKeyPressed(Keyboard::Up)) dir = 0;
+  if(Keyboard::isKeyPressed(Keyboard::Right)) dir = 1;
+  if(Keyboard::isKeyPressed(Keyboard::Down)) dir = 2;
+  if(Keyboard::isKeyPressed(Keyboard::Left)) dir = 3;
+  
+  app.clear();
+
+  for (int i = 0; i < num; i++) {
+    snake.setPosition(s[i].x*size,s[i].y*size);
+    app.draw(snake);
+  }
+
+  fruit.setPosition(f.x*size, f.y*size);
+  app.draw(fruit);
+  
+  app.display();
+  
   return 0;
 }
